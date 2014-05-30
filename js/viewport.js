057382558519ac17421cf2eca038e5188ab317d2
Maciej Małecki <me@mmalecki.com>
2014-05-30 15:21:02 +0200

    Start actually drawing things

--- js/viewport.js
+++ js/viewport.js
@@ -1,11 +1,39 @@
 var degToRad = require('./deg-to-rad.js')
+var shc = require('string-hash-colour')
 
-var Viewport = module.exports = function (canvas) {
+function hashStringToColor(string) {
+  return shc.convert(string, { avoid: '#000000', proximity: 100 })
+}
+
+var Viewport = module.exports = function (canvas, data) {
   this.context = canvas.getContext('2d')
+  this.data = data
 
+  this.data.on('update', this._onDataUpdate.bind(this))
   window.addEventListener('resize', this._resizeCanvas.bind(this))
   window.addEventListener('deviceorientation', this._onOrientation.bind(this), true)
+
   this._resizeCanvas()
+  this._requestAnimationFrame()
+}
+
+Viewport.prototype._requestAnimationFrame = function () {
+  this._redraw()
+  window.requestAnimationFrame(this._onAnimationFrameRequest.bind(this))
+}
+
+Viewport.prototype._onDataUpdate = function (update) {
+  if (update.x > this._maxX) this._maxX = update.x
+  if (update.y > this._maxY) this._maxY = update.y
+  if (update.x < this._minX) this._minX = update.x
+  if (update.y < this._minY) this._minY = update.y
+}
+
+Viewport.prototype._theirsToOurs = function (point) {
+  return {
+    x: (point.x / (this._maxX - this._minX)) * this.width,
+    y: (point.y / (this._maxY - this._minY)) * this.height
+  }
 }
 
 Viewport.prototype._onOrientation = function (orientation) {
@@ -18,6 +46,15 @@ Viewport.prototype._resizeCanvas = function () {
   this.height = canvas.height = window.innerHeight
 }
 
+Viewport.prototype._drawMap = function () {
+  this.data.forEach(function (point) {
+    var coords = this._theirsToOurs(point)
+    this.context.fillStyle = hashStringToColor(point.player)
+    this.context.arc(coords.x, coords.y, 10, 0, 2 * Math.PI)
+    this.context.fill()
+  }, this)
+}
+
 Viewport.prototype._redraw = function () {
   this.context.fillStyle = 'rgb(0, 0, 0)'
   this.context.fillRect(0, 0, this.width, this.height)
@@ -25,8 +62,7 @@ Viewport.prototype._redraw = function () {
   this.context.save()
   this.context.rotate(degToRad(this._alpha))
 
-  this.context.fillStyle = 'rgb(255, 255, 255)'
-  this.context.fillRect(this.width / 2, this.height / 2, 10, 400)
+  this._drawMap()
 
   this.context.restore()
 }
