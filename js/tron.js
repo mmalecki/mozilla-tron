54f98039b0f99ece192c741fa0b6c9d4f5227577
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-30 15:30:31 +0200

    pass movement to viewport

--- js/tron.js
+++ js/tron.js
@@ -2,8 +2,10 @@ var Viewport = require('./viewport.js')
 
 var canvas = document.getElementById('canvas')
 
+var moves = require('./movement')
+
 function start() {
-  var viewport = new Viewport(canvas)
+  var viewport = new Viewport(canvas, moves)
 }
 
 start()
