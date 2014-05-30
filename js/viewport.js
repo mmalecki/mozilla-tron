d52d3c980876fa239d96f52cc633c87778f1d1c0
Maciej Małecki <me@mmalecki.com>
2014-05-30 10:44:51 +0200

    Resize canvas in viewport

--- js/viewport.js
+++ js/viewport.js
@@ -1,2 +1,11 @@
 var Viewport = module.exports = function (canvas) {
-};
+  this.context = canvas.getContext('2d')
+
+  window.addEventListener('resize', this._resizeCanvas.bind(this))
+  this._resizeCanvas()
+}
+
+Viewport.prototype.resizeCanvas = function () {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
